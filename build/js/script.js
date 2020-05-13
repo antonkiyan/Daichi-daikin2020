4ddb921441f226bd4433c152d8dd00bac17b3805
Anton Kiyan <kiyananton@gmail.com>
2020-05-13 17:29:02 +0300

    Исправляет высоту вьюпорта

--- build/js/script.js
+++ build/js/script.js
@@ -280,6 +280,37 @@
 })();
 
 (function () {
+  let vh = window.innerHeight * 0.01;
+
+  var debounce = function (onDelay, delay) {
+    var timeoutId = 0;
+    return function () {
+      var params = arguments;
+
+      if (timeoutId > 0) {
+        clearTimeout(timeoutId);
+      }
+
+      var onTimeout = function () {
+        onDelay.apply(null, params);
+      };
+
+      timeoutId = setTimeout(onTimeout, delay);
+    };
+  };
+
+  var onResize = function () {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    console.log(window.innerHeight);
+
+  };
+
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  window.addEventListener('resize', debounce(onResize, 1000));
+})();
+
+(function () {
   var nav = document.querySelector('.mobile-nav');
   var body = document.querySelector('body');
 
