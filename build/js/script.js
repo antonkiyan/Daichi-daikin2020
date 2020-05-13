5024e2c460483cf1a55445c562e07f0fcfddcf7b
Anton Kiyan <kiyananton@gmail.com>
2020-05-13 17:52:07 +0300

    Убирает обработчик ресайза

--- build/js/script.js
+++ build/js/script.js
@@ -282,30 +282,30 @@
 (function () {
   let vh = window.innerHeight * 0.01;
 
-  var debounce = function (onDelay, delay) {
-    var timeoutId = 0;
-    return function () {
-      var params = arguments;
+  // var debounce = function (onDelay, delay) {
+  //   var timeoutId = 0;
+  //   return function () {
+  //     var params = arguments;
 
-      if (timeoutId > 0) {
-        clearTimeout(timeoutId);
-      }
+  //     if (timeoutId > 0) {
+  //       clearTimeout(timeoutId);
+  //     }
 
-      var onTimeout = function () {
-        onDelay.apply(null, params);
-      };
+  //     var onTimeout = function () {
+  //       onDelay.apply(null, params);
+  //     };
 
-      timeoutId = setTimeout(onTimeout, delay);
-    };
-  };
+  //     timeoutId = setTimeout(onTimeout, delay);
+  //   };
+  // };
 
-  var onResize = function () {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
+  // var onResize = function () {
+  //   let vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // };
 
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-  window.addEventListener('resize', debounce(onResize, 1000));
+  //window.addEventListener('resize', onResize);
 })();
 
 (function () {
